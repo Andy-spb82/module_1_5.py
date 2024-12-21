94a0bb19854c8a7cc6c20024620437c7526bd9e5
Andy-spb82 <Antikaspb2310@gmail.com>
2024-12-21 12:18:09 +0300

    Add files via upload

---
+++ module_1_5.py
@@ -0,0 +1,7 @@
+immutable_var =1,2,3,5,'true',"dog"
+print(immutable_var)
+mutable_list = ([1,2,3,5,'true',"dog"],1,2)
+print(mutable_list )
+mutable_list [0] [2] =  'green'
+mutable_list [0] [4] =  'grey'
+print(mutable_list )
